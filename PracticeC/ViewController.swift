8a4e5cc5c8d768f582604f84ddcb391cc5562d06
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-08-24 10:32:06 +0530

    Initial Commit

---
+++ PracticeC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeC
+//
+//  Created by Rimnesh Fernandez on 24/08/19.
+//  Copyright © 2019 Rimnesh Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
